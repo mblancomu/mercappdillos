ff73bb82ed71ac436a6434bb3d8e1ed7768c0eb2
Manuel Blanco <mblancomu@gmail.com>
2015-09-16 20:41:42 +0200

    added new code for json

--- app/src/main/java/com/mcomobile/mercadillos/DetalleLeer.java
+++ app/src/main/java/com/mcomobile/mercadillos/DetalleLeer.java
@@ -238,4 +238,85 @@ public void onRestart() {
     super.onRestart();
     comprobarConexion();
 }
+
+	/*protected void showList(){
+		try {
+			JSONObject jsonObj = new JSONObject(myJSON);
+			peoples = jsonObj.getJSONArray(TAG_RESULTS);
+
+			for(int i=0;i<peoples.length();i++){
+				JSONObject c = peoples.getJSONObject(i);
+				String id = c.getString(TAG_ID);
+				String name = c.getString(TAG_NAME);
+				String address = c.getString(TAG_ADD);
+
+				HashMap<String,String> persons = new HashMap<String,String>();
+
+				persons.put(TAG_ID,id);
+				persons.put(TAG_NAME,name);
+				persons.put(TAG_ADD,address);
+
+				personList.add(persons);
+			}
+
+			ListAdapter adapter = new SimpleAdapter(
+					MainActivity.this, personList, R.layout.list_item,
+					new String[]{TAG_ID,TAG_NAME,TAG_ADD},
+					new int[]{R.id.id, R.id.name, R.id.address}
+			);
+
+			list.setAdapter(adapter);
+
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void getData(){
+		class GetDataJSON extends AsyncTask<String, Void, String>{
+
+			@Override
+			protected String doInBackground(String... params) {
+				DefaultHttpClient httpclient = new DefaultHttpClient(new BasicHttpParams());
+				HttpPost httppost = new HttpPost("http://simplifiedcoding.16mb.com/get-data.php");
+
+				// Depends on your web service
+				httppost.setHeader("Content-type", "application/json");
+
+				InputStream inputStream = null;
+				String result = null;
+				try {
+					HttpResponse response = httpclient.execute(httppost);
+					HttpEntity entity = response.getEntity();
+
+					inputStream = entity.getContent();
+					// json is UTF-8 by default
+					BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
+					StringBuilder sb = new StringBuilder();
+
+					String line = null;
+					while ((line = reader.readLine()) != null)
+					{
+						sb.append(line + "\n");
+					}
+					result = sb.toString();
+				} catch (Exception e) {
+					// Oops
+				}
+				finally {
+					try{if(inputStream != null)inputStream.close();}catch(Exception squish){}
+				}
+				return result;
+			}
+
+			@Override
+			protected void onPostExecute(String result){
+				myJSON=result;
+				showList();
+			}
+		}
+		GetDataJSON g = new GetDataJSON();
+		g.execute();
+	}*/
 }
